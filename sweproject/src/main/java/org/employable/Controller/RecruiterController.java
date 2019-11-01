d888e2e2cc27c914c7d4eeb602cd2e8820ff3c2f
julianxcarter <juliancarter55@gmail.com>
2019-10-31 22:08:34 -0400

    MongoDb connection & Maven architecture + Lani's JobListingModel changes

---
+++ sweproject/src/main/java/org/employable/Controller/RecruiterController.java
@@ -0,0 +1,41 @@
+package org.employable.Controller;
+
+import org.employable.Model.JobListingModel;
+import org.employable.Model.Recruiter;
+import org.employable.View.RecruiterView;
+
+
+
+public class RecruiterController {
+  private Recruiter model;
+  private RecruiterView view;
+  private JobListingModel jlModel;
+
+  public RecruiterController(Recruiter model, RecruiterView view){
+    this.model = model;
+    this.view = view;
+    //this.jlModel = jobListingModel;
+  }
+
+  public void setName (String name){
+    model.setName(name);
+  }
+
+  public void setPassword(String password){
+    model.setPassword(password);
+  }
+
+  public void setContactInfo(String email){
+    model.setContactInfo(email);
+  }
+
+  public void createJobListing(String position, String company, String link, String location){
+    JobListingModel model = new JobListingModel(position, company, link, location);
+  }
+
+  //method to remove JobListing
+  // public void removeJobListing(JobListingModel expiredJob){
+  //   expiredJob.removeJobListing(expiredJob);
+  // }
+  //show new interaction/notificiation. Dev later.
+}
