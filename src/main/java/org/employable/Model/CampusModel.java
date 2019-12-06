65c34bdeeeb358e6fa948b6fbc62b0c12c563b49
julianxcarter <juliancarter55@gmail.com>
2019-12-06 12:38:33 -0500

    Quick issue fixes

--- src/main/java/org/employable/Model/CampusModel.java
+++ src/main/java/org/employable/Model/CampusModel.java
@@ -1,6 +1,6 @@
 package org.employable.Model; //CampusModel is in the model package
 
-public class CampusModel extends Company{
+public class CampusModel extends CompanyModel{
   String campusLocation;
   String [] mediaIds = new String[10];
   String[] amenities = new String [40];
