aed56b4f828bc2cdc79d3a731a5ae35054eda504
Thulani Vereen <thulanivereen@Thulanis-MacBook-Air-6.local>
2019-12-06 11:36:10 -0500

    changing the name of the class to match filename

--- src/main/java/org/employable/Model/CompanyModel.java
+++ src/main/java/org/employable/Model/CompanyModel.java
@@ -1,14 +1,14 @@
 package org.employable.Model; //JobListingModel is in the model package
 
-public class Company {
+public class CompanyModel {
   String companyName;
   String companyMediaId; //Representing mediaId for company Logo
   Integer companyRating; //used to be CompanyRating
   String companyPolicy; //could be text of company policy or representing the company's policy url
   String[] companyLocations = new String[50];
 
-  public Company(){};
-  public Company (String name, String mediaId, String policy, String [] locations)
+  public CompanyModel(){};
+  public CompanyModel (String name, String mediaId, String policy, String [] locations)
   {
     this.companyName = name;
     this.companyMediaId = mediaId;
