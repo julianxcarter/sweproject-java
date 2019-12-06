65ca154219870a091006b4a09b1b9544236f1ad4
Thulani Vereen <thulanivereen@Thulanis-MacBook-Air-6.local>
2019-12-06 12:30:40 -0500

    removing rating from campus class

--- src/main/java/org/employable/Model/CampusModel.java
+++ src/main/java/org/employable/Model/CampusModel.java
@@ -4,7 +4,6 @@ public class CampusModel extends Company{
   String campusLocation;
   String [] mediaIds = new String[10];
   String[] amenities = new String [40];
-  Integer campusRating;
   public CampusModel (String[] pictures, String[] amenities){
     this.mediaIds = pictures;
     this.amenities = amenities;
