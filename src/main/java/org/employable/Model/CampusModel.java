224516cd3fac09b04bc2e32d010d1d37aab69ddf
Thulani Vereen <thulanivereen@Thulanis-MacBook-Air-6.local>
2019-12-06 12:37:51 -0500

    adding methods for campus page

--- src/main/java/org/employable/Model/CampusModel.java
+++ src/main/java/org/employable/Model/CampusModel.java
@@ -1,4 +1,5 @@
 package org.employable.Model; //CampusModel is in the model package
+import java.util.List;
 
 public class CampusModel extends Company{
   String campusLocation;
@@ -7,5 +8,17 @@ public class CampusModel extends Company{
   public CampusModel (String[] pictures, String[] amenities){
     this.mediaIds = pictures;
     this.amenities = amenities;
+  }
+
+  public List<String> getAmenities(){
+    return this.amenities;
+  }
+
+  public void addAmenity(String amenity){
+    this.amenities.add(amenity);
+  }
+
+  public void addMedia(String media){
+    this.mediaIds.add(media);
   }
 }
